824c2b9539478595d5475ce2183528f06267a3ca
kurarrr <y.kurarrr@gmail.com>
2019-11-20 11:18:37 +0900

    comparison due to use_cu1

--- test/benchmark/quantum_fourier_transform_fusion_benchmarks.py
+++ test/benchmark/quantum_fourier_transform_fusion_benchmarks.py
@@ -1,29 +1,33 @@
-import math
-import sys
+import math, itertools
 from qiskit import QuantumRegister, ClassicalRegister, QuantumCircuit
 from qiskit.compiler import assemble, transpile
 from qiskit.providers.aer import QasmSimulator
 
-def qft(qreg, circuit, use_cu1):
-    n = len(qreg)
-    for i in range(n):
+def qft_circuit(num_qubit, use_cu1):
+    qreg = QuantumRegister(num_qubit,"q")
+    creg = ClassicalRegister(num_qubit, "c")
+    circuit = QuantumCircuit(qreg, creg)
+
+    for i in range(num_qubit):
         circuit.h(qreg[i])
 
-    if use_cu1:
-        for i in range(n):
-            for j in range(i):
-                circuit.cu1(math.pi/float(2**(i-j)), qreg[i], qreg[j])
-            circuit.h(qreg[i])
-    else:
-        for i in range(n):
-            for j in range(i):
-                l = math.pi/float(2**(i-j))
+    for i in range(num_qubit):
+        for j in range(i):
+            l = math.pi/float(2**(i-j))
+            if use_cu1:
+                circuit.cu1(l, qreg[i], qreg[j])
+            else:
                 circuit.u1(l/2, qreg[i])
                 circuit.cx(qreg[i], qreg[j])
                 circuit.u1(-l/2, qreg[j])
                 circuit.cx(qreg[i], qreg[j])
                 circuit.u1(l/2, qreg[j])
-            circuit.h(qreg[i])
+        circuit.h(qreg[i])
+
+    circuit.barrier()
+    for i in range(num_qubit):
+        circuit.measure(q[i], c[i])
+    self.qft_circuits.append(qobj)
 
     return circuit
 
@@ -32,25 +36,20 @@ class QuantumFourierTransformFusionSuite:
         self.timeout = 60 * 20
         self.qft_circuits = []
         self.backend = QasmSimulator()
-        for num_qubits in (5, 10, 15, 20, 25, 26):
+        num_qubits = [5, 10, 15, 20, 25, 26]
+
+        for num_qubit in num_qubits:
             for use_cu1 in [True, False]:
-                q = QuantumRegister(num_qubits,"q")
-                c = ClassicalRegister(num_qubits, "c")
-                circ = qft(q, QuantumCircuit(q, c, name="qft"), use_cu1)
-                circ.barrier()
-                for i in range(num_qubits):
-                    circ.measure(q[i], c[i])
-                qobj = assemble(circ)
-                self.qft_circuits.append(qobj)
+                circuit = qft_circuit(num_qubit, use_cu1)
+                self.circuit[(num_qubit, use_cu1)] = assemble(circuit, self.backend, shots=1)
+        self.param_names = ["Quantum Fourier Transform", "Fusion Activated", "Use cu1 gate"]
+        self.params = (num_qubits, [True, False], [True, False])
 
-        self.param_names = ["Quantum Fourier Transform", "Fusion Activated"]
-        self.params = (self.qft_circuits, [True, False])
-
-    def setup(self, qobj, fusion_enable, use_cu1):
+    def setup(self, num_qubit, fusion_enable, use_cu1):
         """ Setup env before benchmarks start """
 
-    def time_quantum_fourier_transform(self, qobj, fusion_enable):
+    def time_quantum_fourier_transform(self, num_qubit, fusion_enable, use_cu1):
         """ Benchmark QFT """
-        result = self.backend.run(qobj, backend_options={'fusion_enable': fusion_enable}).result()
+        result = self.backend.run(self.circuit[(num_qubit, use_cu1)], backend_options={'fusion_enable': fusion_enable}).result()
         if result.status != 'COMPLETED':
             raise QiskitError("Simulation failed. Status: " + result.status)
